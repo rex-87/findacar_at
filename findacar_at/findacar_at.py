8c01a0505319780a4263e79fd050e3cb29ad1b0a
Romain Thibault <rex87@orange.fr>
2020-08-25 23:33:11 +0100

    more features; web scraping can be tedious ...

--- findacar_at/findacar_at.py
+++ findacar_at/findacar_at.py
@@ -32,9 +32,33 @@ try:
 	import bs4
 	import webbrowser
 	from requests_html import HTMLSession
-	
-	search_url = r'https://www.autotrader.co.uk/car-search?sort=distance&postcode=gu213bn&radius=15&price-from=2500&price-to=3500&year-from=2010&year-to=2015&maximum-mileage=52000&fuel-type=Petrol&quantity-of-doors=5&fuel-consumption=OVER_50'
 
+	post_code = 'gu216yl'
+	min_price = 4000
+	max_price = 6000
+	min_mileage = 0
+	max_mileage = 57000
+	min_year = 2010
+	max_year = 2016
+	this_year = 2020
+	
+	search_url = r'https://www.autotrader.co.uk/car-search?postcode={post_code}&radius=15&price-from={min_price}&price-to={max_price}&year-from={min_year}&year-to={max_year}&maximum-mileage={max_mileage}&fuel-type=Petrol&quantity-of-doors=5&fuel-consumption=OVER_50'.format(
+		post_code = post_code,
+		min_price = min_price,
+		max_price = max_price,
+		max_mileage = max_mileage,
+		min_year = min_year,
+		max_year = max_year,
+	)
+	
+	min_age = this_year - max_year
+	max_age = this_year - min_year
+	
+	price_range = max_price - min_price
+	age_range = max_age - min_age
+	mileage_range = max_mileage - min_mileage
+
+	print("URL:", search_url)
 	print("Get page count ...")
 	r = requests.get(search_url)
 	search_soup = bs4.BeautifulSoup(r.content, features="html.parser")
@@ -43,33 +67,56 @@ try:
 	)
 
 	ids = []
-	for page in range(page_count):
+	cars = []
+	for page in range(1, page_count + 1):
 		print("Get ids from page {} ...".format(page))
 		r = requests.get(search_url+'&page={}'.format(page))		
 		search_soup = bs4.BeautifulSoup(r.content, features="html.parser")
-		ids.extend(
-			[li['id'] for li in search_soup.find_all('li') if 'class' in li.attrs and 'search-page__result' in li['class'] and 'data-is-promoted-listing' not in li.attrs and 'data-is-ymal-listing' not in li.attrs]
-		)
-
-	car_urls = ['https://www.autotrader.co.uk/car-details/{}'.format(id) for id in ids]
-	for car_url in car_urls:
-		# webbrowser.open(car_url)
-		session = HTMLSession()
-		r = session.get(car_url)
-		r.html.render()
-		car_soup = bs4.BeautifulSoup(r.html.raw_html, features="html.parser")
 		
-		price = int(
-			car_soup.find('h2', class_ = 'price-confidence__price').text.replace('Price £', '').replace(',', '')
-		)
-		mileage = car_soup.find('span', class_ = 'mileage__distance').text
-		age = car_soup.find('p', class_ = 'advert-heading__year-title').text
+		this_page_ids = [li['id'] for li in search_soup.find_all('li') if 'class' in li.attrs and 'search-page__result' in li['class'] and 'data-is-promoted-listing' not in li.attrs and 'data-is-ymal-listing' not in li.attrs]
 
+		for id_ in this_page_ids:
+			id_li = search_soup.find('li', id = id_)
+			
+			spec_lis = id_li.find_all('li')
+			if 'CAT' in spec_lis[0].text:
+				continue
 
-		It will be quicker to do the scraping from the search page itself instead of the actual car page which needs javascript to be rendered properly.
+			try:
+				price = int(id_li.find('div', class_ = 'vehicle-price').text.replace('£', '').replace(',', ''))
+			except:
+				print('*** THERE WAS AN EXCEPTION ***')
+				import IPython; IPython.embed(colors='Neutral')
+			year = int(spec_lis[0].text.split(' ')[0])
+			mileage = int(spec_lis[2].text.replace(' miles', '').replace(',', ''))
+			distance = int(id_li.get('data-distance-value').split(' ')[0])
 
+			age = this_year-year
+			norm_price = (price - min_price)/price_range
+			norm_age = (age - min_age)/age_range
+			norm_mileage = (mileage - min_mileage)/mileage_range
+			cars.append(
+				{
+					'id' : id_,
+					'price' : price,
+					'year' : year,
+					'mileage' : mileage,
+					'distance' : distance,
+					'norm_price' : norm_price,
+					'norm_age' : norm_age,
+					'norm_mileage' : norm_mileage,
+					'score' : 1/3*(norm_price + norm_mileage + norm_age),
+				}
+			)
+
+		ids.extend(this_page_ids)
+
+	sorted_cars = sorted(cars, key=lambda car: car['score'])
+
+	for car in sorted_cars:
+		webbrowser.open('https://www.autotrader.co.uk/car-details/{}'.format(car['id']))
+		print(car)
 		import IPython; IPython.embed(colors='Neutral')
-
 
 ## -------- SOMETHING WENT WRONG -----------------------------	
 except:
